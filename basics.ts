51a16c660b9a72dafecd468a231197263632dfba
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-07-20 18:29:50 +0530

    Chai Aur TypeScript Series Started.First Episode Completed

--- basics.ts
+++ basics.ts
@@ -1,12 +1,28 @@
 //&How do we write functions in Javascript
-function greet(name) {
+//?name: string means whatever parameter I will give in the greet function,it will be of the type: string.now name value that I am expecting will be the string only.
+
+//?What value will this function will return I can also tell that by Putting Up a colon.That too I want string.After round paranthesis write :string
+function greet(name: string): string {
   return `Hello ${name}`;
 }
 
 console.log(greet("Keshav"));
-//?This code will work but there is a problem with this code i.e if you changed the datatype here from "Keshav" to true.The code will run successfully but not to our expectations.
-console.log(greet(true));
-//~What if I pass a number datatype over here?
-console.log(greet(42));
-//?See the greet function was expecting that you will pass a string inside it,but you can pass a boolean as well as the number data type as well.That is why Typescript comes into the picture.
-//&Basically When you give Javascript some extra super special Powers.That is called Typescript.But the moment when you change the extension from js to TS and you try to see the same code,you will start noticing some errors.
+// console.log(greet(42)); Even if you run this code with the command node basics.ts, the code will be run successfully even though I was seeing the warnings because node basically removes all the types.
+//?What is there extra in the TypeScript ?? i.e types i.e data-types.
+
+//?In the Programming Languages like Java, C, C++, in these basically you always define a data type like int, string, float, boolean etc.But in Javascript you do'nt do anything like that.
+
+//&As the Application grows up types helps us to recongnise the error early.
+
+//?Another Problem is the loose docs.
+
+//?Developer Tooling
+
+//?AI
+
+//~Advantages Of Typescript ==>
+//~It is addon on to top of the Javascript.
+//~Typescript never runs.Basically TypeScript needs a process,when this process happens then at the end of the day you get the JS.
+//~ TS ==> Process ==> JS
+//~Typescript has the type Checkers, which helps in checking the types,it also sees the consistency of the types.
+//~With the help of type Checkers you get the consistency in the Code.
